0e0dff6c595681d409ff36a0bda6146bb42d8f8d
Gustavo-Ae <gustavoaedias05@hotmail.com>
2021-12-01 16:27:53 -0300

    Criando a tela de login e adionando o status();

---
+++ gestaoServico/src/connection/ConnectionFactory.java
@@ -0,0 +1,68 @@
+
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class ConnectionFactory{
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+
+    private static final String URL = "jdbc:mysql://localhost/dbinfo";
+
+    private static final String USER = "root";
+
+    private static final String PASS = "";
+        
+    public static Connection getConexao(){
+        try{
+            Class.forName(DRIVER);
+            
+            return DriverManager.getConnection(URL, USER, PASS);
+        }catch(Exception erro){
+            System.out.println(erro.getMessage()); // mostra o erro;
+            erro.printStackTrace(); // mostra a linha que está o erro;
+            return null;
+        }      
+    }
+    
+    public static void fecharConexao(Connection con){
+        try{
+            if(con != null){       
+                con.close();
+            }
+        }catch(SQLException sql){
+            throw new RuntimeException("Houve um erro no SQL, no método closeConnection() :",sql); 
+        }
+    }
+    
+    public static void fecharConexao(Connection con, PreparedStatement acessoBD){
+         
+        fecharConexao(con);
+         
+        try {
+             if(acessoBD != null){       
+                acessoBD.close();
+            }
+        }catch (SQLException sql){
+            throw new RuntimeException("Houve um erro no SQL, no método closeConnection() :",sql); 
+        }
+    }
+    
+    public static void fecharConexao(Connection con, PreparedStatement acessoBD, ResultSet rs){
+         
+        fecharConexao(con,acessoBD);
+         
+        try {
+            if(rs != null){       
+                rs.close();
+            }
+        }catch (SQLException sql){
+            throw new RuntimeException("Houve um erro no SQL, no método closeConnection() :",sql); 
+        }
+    }
+}
+
---
+++ gestaoServico/src/view/TelaLogin.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import connection.ConnectionFactory;
+import java.sql.Connection;
+import javax.swing.ImageIcon;
+
+
+public class TelaLogin extends javax.swing.JFrame {
+
+    
+    public TelaLogin() {
+        initComponents();
+        status();
+    }
+
+    public void status(){
+        Connection conexao = ConnectionFactory.getConexao();
+        
+        if(conexao == null){
+            jLabel_status.setIcon(new ImageIcon(getClass().getResource("/icones/serverOFF.png")));
+        }else{
+            jLabel_status.setIcon(new ImageIcon(getClass().getResource("/icones/serverON.png")));
+        }
+    }
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jTextField_usuario = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField_senha = new javax.swing.JTextField();
+        jLabel_status = new javax.swing.JLabel();
+        jButton_entrar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Tela Login");
+        setResizable(false);
+
+        jLabel1.setText("Usuário: ");
+
+        jLabel2.setText("Senha:");
+
+        jLabel_status.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icones/serverON.png"))); // NOI18N
+
+        jButton_entrar.setText("Entrar");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addComponent(jLabel_status)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton_entrar)
+                .addGap(28, 28, 28))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(101, 101, 101)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(19, 19, 19)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jTextField_senha)
+                    .addComponent(jTextField_usuario, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(126, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(60, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel_status)
+                        .addGap(20, 20, 20))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(jTextField_usuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(36, 36, 36)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(jTextField_senha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(46, 46, 46)
+                        .addComponent(jButton_entrar)
+                        .addGap(41, 41, 41))))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaLogin().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton_entrar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel_status;
+    private javax.swing.JTextField jTextField_senha;
+    private javax.swing.JTextField jTextField_usuario;
+    // End of variables declaration//GEN-END:variables
+}
