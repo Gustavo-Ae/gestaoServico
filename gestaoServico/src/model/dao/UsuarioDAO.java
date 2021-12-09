989b0a8271aaaa373cef4c9a38331c7f1b79440b
Gustavo-Ae <gustavoaedias05@hotmail.com>
2021-12-09 11:09:54 -0300

    criando o botão para alterar usuário

--- gestaoServico/src/model/dao/UsuarioDAO.java
+++ gestaoServico/src/model/dao/UsuarioDAO.java
@@ -3,11 +3,10 @@ package model.dao;
 
 import connection.ConnectionFactory;
 import controller.Usuario;
-import java.util.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 
@@ -35,6 +34,34 @@ public class UsuarioDAO {
         }catch(Exception e){
             System.out.println("Erro no método insert() -> "+e.getMessage());
             e.printStackTrace();
+        }finally{
+            ConnectionFactory.fecharConexao(con, acessoBD);
+        }   
+    }
+     
+    public void update(Usuario usuario){
+        
+        Connection con = ConnectionFactory.getConexao();
+       
+        PreparedStatement acessoBD = null;
+        
+        try{
+            acessoBD = con.prepareStatement("UPDATE tbusuario SET nome = ?, fone = ?, usuario = ?, senha = ?, perfil = ? WHERE idUser = ?");
+            
+            acessoBD.setString(1, usuario.getNome());
+            acessoBD.setString(2, usuario.getFone());
+            acessoBD.setString(3, usuario.getUsuario());
+            acessoBD.setString(4, usuario.getSenha());
+            acessoBD.setString(5, usuario.getPerfil());
+            acessoBD.setInt(6, usuario.getId());
+            
+            acessoBD.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Atualizado com sucesso","Aviso", JOptionPane.INFORMATION_MESSAGE);
+            
+        }catch(SQLException sql){
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar o banco de dados","ERRO",JOptionPane.ERROR_MESSAGE);
+            throw new RuntimeException("Houve um erro no método update() :",sql);    
         }finally{
             ConnectionFactory.fecharConexao(con, acessoBD);
         }   
